f9d16b3e49c3cbdd368de73c1722aaf47c1a592e
U-BSD\463541 <463541@SHS-CYBF6X3.beaverton.k12.or.us>
2023-10-05 10:08:42 -0700

    defined struct

--- studentList.cpp
+++ studentList.cpp
@@ -1,19 +1,17 @@
 //C++ Programming: Student List Project
-// by Akhil Baidya
-
-//Credits: Mr. Galbraith's videos on Canvas (on Structs)
+//by Akhil Baidya
 
 #include <iostream>
-
 using namespace std;
 
-struct Student { //code for Struct based on Mr. Galbraith's video on Structs in Canvas
-  char firstName[20];
-  char lastName[20];
+struct Student {
+
+  char firstName[20]; //Struct code based on Mr. Galbraith's video on Structs in Canvas
+  char secondName[20];
   int id;
-  float gpa; //a student has first name, last name, id, and gpa
+  int gpa; //a Student has a first name, last name, id, and gpa 
 }
-  
+
 int main() {
   return 0;
 }
