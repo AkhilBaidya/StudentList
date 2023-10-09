bf786f4b6f2c1fb1d4579b7ef72d50f288b2ddbc
U-BSD\463541 <463541@SHS-CYBF6X3.beaverton.k12.or.us>
2023-10-09 10:08:35 -0700

    ADD THIS.

--- studentList.cpp
+++ studentList.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 using namespace std;
 
+//DEFINING STRUCTS:
+
 struct Student {
 
   char firstName[20]; //Struct code based on Mr. Galbraith's video on Structs in Canvas
@@ -12,6 +14,41 @@ struct Student {
   float gpa; //a Student has a first name, last name, id, and gpa 
 }
 
+
+//FUNCTION PROTOTYPES:
+
+void ADD(&vector<Student*> newStudVec);
+void PRINT(&vector<Student*> newStudVec);
+void DELETE(&vector<Student*> newStudVec);
+  
+
+//MAIN FUNCTION:
+
 int main() {
+  bool running = true;
+  
+  vector<Student*> studVec;
+
+
+  while (running) {
+
+
+  }
+  
+
   return 0;
+}
+
+//OTHER FUNCTIONS IMPLEMENTED IN MAIN FUNCTION:
+
+void ADD(&vector<Student*> newStudVec) {
+
+}
+
+void PRINT(&vector<Student*> newStudVec) {
+
+}
+
+void DELETE(&vector<Student*> newStudVec) {
+
 }
